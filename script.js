fd57ffd58c30e218e00421f03b84d25fc318e7a2
nkatiecracks <yoanamensah@gmail.com>
2026-05-20 18:11:11 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,1 @@
+const menuToggle=document.querySelector(".menu-toggle");const nav=document.querySelector(".nav");if(menuToggle&&nav){menuToggle.addEventListener("click",()=>{nav.classList.toggle("open")})}
